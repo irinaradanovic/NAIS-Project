e7542d4dd6854651806ebf6457b1f4a7fcd4ebd7
irinaradanovic <irina.radanovic5@gmail.com>
2026-05-31 22:47:46 +0200

    add: redis to restaurant timeseries service

--- RestaurantTimeSeriesService/src/main/java/rs/ac/uns/acs/nais/RestaurantTimeSeriesService/service/MenuStatusEventService.java
+++ RestaurantTimeSeriesService/src/main/java/rs/ac/uns/acs/nais/RestaurantTimeSeriesService/service/MenuStatusEventService.java
@@ -1,6 +1,9 @@
 package rs.ac.uns.acs.nais.RestaurantTimeSeriesService.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.acs.nais.RestaurantTimeSeriesService.dto.MenuCategoryActivityDTO;
 import rs.ac.uns.acs.nais.RestaurantTimeSeriesService.model.MenuStatusEvent;
@@ -11,14 +14,18 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MenuStatusEventService {
 
     private final MenuStatusEventRepositoryImpl repository;
 
+    // Whenever theres a new entry, delete cache because the response wont be the same
+    @CacheEvict(value = "categoryActivity", allEntries = true)
     public void save(MenuStatusEvent event) {
         if (event.getTime() == null) {
             event.setTime(Instant.now()); // if time is not set, set it to current time
         }
+        log.info("[CACHE EVICT] Clearing cache.");
 
         repository.save(event);
     }
@@ -27,7 +34,9 @@ public class MenuStatusEventService {
         repository.saveAll(events);
     }
 
+    @CacheEvict(value = "categoryActivity", allEntries = true)
     public boolean deleteByMenuId(String menuId) {
+        log.info("[CACHE EVICT] Clearing cache.");
         return repository.deleteByMenuId(menuId);
     }
 
@@ -39,7 +48,15 @@ public class MenuStatusEventService {
         return repository.findAllByMenuId(menuId);
     }
 
+    @Cacheable(value = "categoryActivity", key = "#minNumberOfVersions")
     public List<MenuCategoryActivityDTO> categoryActivity(int  minNumberOfVersions) {
+        log.info("[CACHE] Querying InfluxDB for categoryActivity, minVersions={}",
+                minNumberOfVersions);
         return repository.findFrequentCategoryChanges(minNumberOfVersions);
+    }
+
+    @CacheEvict(value = "categoryActivity", allEntries = true)
+    public void evictCategoryActivityCache() {
+        log.info("[CACHE EVICT] Clearing categoryActivity cache");
     }
 }
